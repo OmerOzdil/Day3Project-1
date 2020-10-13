0a8c7a388e659655b6b93e5cc44aebaf5e405da6
Omer Ozdil <omerozdil54@gmail.com>
2020-10-13 17:15:24 +0100

    added to branch1

--- src/test/java/folder/Hello.java
+++ src/test/java/folder/Hello.java
@@ -3,5 +3,8 @@ package folder;
 public class Hello {
     public static void main(String[] args) {
         // I added from my Intelij.
+        // this is from the branch1
+        // let me try to see this from the origin/master
+
     }
 }
